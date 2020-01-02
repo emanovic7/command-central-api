7220836f3bd63c3ac6318b65764f6c51da020ea1
emanovic7 <e_owusu@live.com>
2020-01-02 18:42:16 -0500

    save app secret in rails credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,20 @@ class ApplicationController < ActionController::API
     request.headers["Authorization"].split(" ")[1]
   end
 
+  def myAppSecret
+    ##ENV['jarvisSecret']
+    Rails.application.credentials.jarvis_secret
+  end
+
   def encode_token(user_id)
     payload = {
       user_id: user_id
     }
-    JWT.encode(payload, "myAppSecret", 'HS256')
+    JWT.encode(payload, myAppSecret, 'HS256')
   end
 
   def decoded_token
-    JWT.decode(token, "myAppSecret", true, { algorithm: 'HS256'})
+    JWT.decode(token, myAppSecret, true, { algorithm: 'HS256'})
   end
 
   def current_user
