a51795f7cbcba7d1c001027bb36e6f1aa64aeccb
emanovic7 <e_owusu@live.com>
2020-01-02 16:51:25 -0500

    remove users index from users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,10 @@ class UsersController < ApplicationController
   end
 
   def index
-    users = User.all
-    render json: users
+    # remove after debugging.
+    
+    # users = User.all
+    # render json: users
   end
 
   def create
@@ -26,3 +28,4 @@ class UsersController < ApplicationController
     end
 
 end
+ De
