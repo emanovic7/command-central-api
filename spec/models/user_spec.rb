9bddf98e7fd5759b45c6feae9c258b7458a3d17c
emanovic7 <e_owusu@live.com>
2020-09-30 18:21:36 -0400

    adds email to users, adds tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :username }
     it { is_expected.to validate_uniqueness_of :username }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_uniqueness_of :email }
   end
 end
 
