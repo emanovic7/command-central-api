e4936e7f3924d49e12f353c29758315e6fe02955
emanovic7 <e_owusu@live.com>
2020-09-30 18:09:39 -0400

    Validations

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   
-  describe 'validations' do 
+  describe 'Validations' do 
    it { is_expected.to validate_presence_of :note }
    it { is_expected.to belong_to :user }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  describe 'validations' do 
+  describe 'Validations' do 
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :username }
     it { is_expected.to validate_uniqueness_of :username }
