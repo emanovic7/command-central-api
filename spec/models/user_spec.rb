32c5ebc23cacffb14c0e6771d9605b745854120d
emanovic7 <e_owusu@live.com>
2020-09-30 18:03:44 -0400

    adds user, task factories, adds simple validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # it 'is valid with a name, username, and password' do 
-  #   user = User.new(
-  #     name: 'Prince',
-  #     username: 'PurplePrince',
-  #     password: 'purpleRain'
-  #   )
-  #   expect(user).to be_valid
 
-  let(:user) { create :user }
-
-    it 'is invalid without a name' do 
-      binding.pry
-      # user = User.new(name: nil)
-      # user.valid?
-      # expect(user.errors[:name]).to include("can't be blank")
-    end
-
-    describe 'validations' do 
-      it { should validate_presence_of(:name) }
-    end
-
+  describe 'validations' do 
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :username }
+    it { is_expected.to validate_uniqueness_of :username }
   end
+end
 
